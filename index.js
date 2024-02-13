56183976c21f9035827021fa796f5886dcbc6f78
rupomhasan <rupom.hasan607299@gmail.com>
2024-02-13 22:14:04 +0600

    Implement product, user , employee update and add to database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { AsyncLocalStorage } = require("async_hooks");
 const port = process.env.PORT || 2500;
 // middleware
 app.use(cors());
@@ -26,15 +27,15 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    client.connect();
 
-    await client.db("admin").command({ ping: 1 });
+    client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
     const clouthCollection = client.db("FashionDB").collection("Products");
-
+    const userCollection = client.db("FashionDB").collection("User");
     // get method
     app.get("/", async (req, res) => {
       res.send("App is running111...");
@@ -53,7 +54,52 @@ async function run() {
       const result = await clouthCollection.findOne(query);
       res.send(result);
     });
+    app.post("/products", async (req, res) => {
+      const products = req.body;
 
+      const result = await clouthCollection.insertOne(products);
+      res.send(result);
+    });
+
+    app.put("/productDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const cloth = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCloth = {
+        $set: {
+          productName: cloth?.productName,
+          productId: cloth?.productId,
+          price: cloth?.price,
+          status: cloth?.status,
+          availableSizes: cloth?.availableSizes,
+          offer: cloth?.offer,
+          productDetails: cloth?.productDetails,
+          productType: cloth?.productType,
+          image: cloth?.image,
+          brandName: cloth?.brandName,
+          gender: cloth?.gender,
+          rating: cloth?.rating,
+          ratingReview: cloth?.ratingReview,
+          dailyDeals: cloth?.dailyDeals,
+        },
+      };
+
+      const result = await clouthCollection.updateOne(filter, updatedCloth);
+      res.send(result);
+    });
+
+    app.delete("/productDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await clouthCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.delete("/products", async (req, res) => {
+      const result = await clouthCollection.deleteMany();
+      res.send(result);
+    });
     // Products Review
 
     const reviewCollection = client
@@ -93,6 +139,31 @@ async function run() {
     });
     app.delete("/addedCart", async (req, res) => {
       const result = await cartCollection.deleteMany();
+      res.send(result);
+    });
+
+    // User
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    const employeeCollection = client.db("FashionDB").collection("Employee");
+    //Employee
+    app.get("/employee", async (req, res) => {
+      const result = await employeeCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/employee/:id", async (req, res) => {
+      const id = req.params.id;
+      const cursor = { _id: new ObjectId(id) };
+      const result = await employeeCollection.findOne(cursor);
       res.send(result);
     });
   } finally {
